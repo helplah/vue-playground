5afb8b38aa386fcf4d1abc992ec7b37f62fc3ab4
Jen <30688276+helplah@users.noreply.github.com>
2018-06-14 21:11:07 +0800

    first vue project

---
+++ order-form/index.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    items: [
+			{ text: "Web Development" },
+			{ text: "Design" },
+			{ text: "Integration" },
+      { text: "testing" }
+		]
+  }
+});
