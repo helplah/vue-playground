e0ad88242454e50c22ba58fc707fe4877f103fc0
Jen <30688276+helplah@users.noreply.github.com>
2018-06-15 22:34:46 +0800

    added methods

--- order-form/index.js
+++ order-form/index.js
@@ -1,11 +1,36 @@
-let app = new Vue({
+Vue.filter("currency", function(value) {
+  return "$" + value.toFixed(2);
+})
+
+const app = new Vue({
   el: '#app',
   data: {
-    items: [
-			{ text: "Web Development" },
-			{ text: "Design" },
-			{ text: "Integration" },
-      { text: "testing" }
-		]
+    // an array of objects
+    services: [
+			{ text: "Web Development",
+        price: 300,
+        active: false
+      },
+      { text: "Design",
+        price: 400,
+        active: false
+      },
+			{ text: "Integration",
+        price: 250,
+        active: false
+      },
+      { text: "testing",
+        price: 220,
+        active: false
+      }
+    ],
+    methods: {
+      toggleActive: function(svc) {
+        svc.active = !svc.active;
+      },
+      total: function() {
+        if svc
+      }
+    }
   }
 });
