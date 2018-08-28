c0d3f2186fc80d01e859621e238a35ef23bdd441
Jen <30688276+helplah@users.noreply.github.com>
2018-08-28 16:22:06 +0800

    came up w solution for assignment

--- vue-assignment6/src/App.vue
+++ vue-assignment6/src/App.vue
@@ -1,39 +1,30 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
+        <Header></Header>
         <hr>
         <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
+            <Servers></Servers>
+            <ServerDetails></ServerDetails>
         </div>
         <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
+        <Footer></Footer>
     </div>
 </template>
 
 <script>
+    import Header from './components/Header.vue'
+    import Servers from './components/Servers.vue'
+    import ServerDetails from './components/ServerDetails.vue'
+    import Footer from './components/Footer.vue'
+
+    export default {
+        components: {
+            Header,
+            Servers,
+            ServerDetails,
+            Footer
+        }
+    }
 </script>
 
 <style>
